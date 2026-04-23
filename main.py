964b03b75a473c65b1738238bb219c399e46e31e
ILIASB1212 <iliasbessiness123456@email.com>
2026-04-23 13:27:23 +0000

    data validations

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exeption.costum_expection import CustomException
 from networksecurity.constant import training_pipeline
 from networksecurity.logging.loger import get_logger
+from networksecurity.components.data_validation import (DataIngestionArtifactes, DataValidation,
+    DataValidationConfig)
 from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
 import sys
 
@@ -20,6 +22,15 @@ if __name__=="__main__":
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
         print(data_ingestion_artifact)
         logger.info("data_ingestion_completed :::::")
+        logger.info("data validation initiated ::::")
+
+        data_validation_config=DataValidationConfig(data_training_pipeline)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        data_validation_artifacts=data_validation.initiate_data_validation()
+        logger.info("data validation completed ::::")
+        print(data_validation_artifacts)
+
+
 
     except Exception as e:
             logger.error(f"Error in main file: {e}")
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -5,6 +5,15 @@ import numpy as np
 import pandas as pd
 
 
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+
+
+
+####################################################################
+#                           DATA ingestion
+####################################################################
+
 DATA_INGESTION_COLLECTION_NAME:str="network_security_collection"
 DATA_INGESTION_DATABASE_NAME:str="ilias_database"
 DATA_INGESTION_DIR_NAME:str="data_ingestion_dir"
@@ -19,3 +28,27 @@ ARTIFACTS_DIR:str="artifacts"
 DATA_FILENAME:str="phisingData.csv"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
+
+
+
+
+####################################################################
+#                           DATA VALIDATION
+####################################################################
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
+####################################################################
+#                        DATA TRANSFORMATION
+####################################################################
+
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
